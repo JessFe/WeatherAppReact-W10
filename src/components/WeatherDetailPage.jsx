726056ad751e4664f7da75d2cf74d2b274911c30
Jess <146346577+JessFe@users.noreply.github.com>
2024-01-12 16:26:02 +0100

    previsioni

--- src/components/WeatherDetailPage.jsx
+++ src/components/WeatherDetailPage.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
+import { Card, Container, Row, Col } from "react-bootstrap";
 
 function WeatherDetailPage() {
   const { city } = useParams();
   const [weatherData, setWeatherData] = useState(null);
+  const [forecastData, setForecastData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -13,8 +15,36 @@ function WeatherDetailPage() {
   // kelv cels
   const kelvinToCelsius = (kelvin) => (kelvin - 273.15).toFixed(2);
 
+  // conv time
+  const timeToLocaleTime = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.toLocaleTimeString();
+  };
+
+  const getDailyForecasts = (forecasts) => {
+    const dailyForecasts = [];
+    const processedDays = new Set();
+    const today = new Date().getUTCDate(); // oggi
+
+    for (const forecast of forecasts.list) {
+      const date = new Date(forecast.dt * 1000);
+      const day = date.getUTCDate();
+
+      if (day !== today && !processedDays.has(day)) {
+        processedDays.add(day);
+        dailyForecasts.push(forecast);
+
+        // 3gg
+        if (dailyForecasts.length === 3) break;
+      }
+    }
+
+    return dailyForecasts;
+  };
+
   useEffect(() => {
     const fetchWeatherData = async () => {
+      setLoading(true);
       try {
         // chiamata geocoding
         const geocodingResponse = await fetch(
@@ -22,18 +52,26 @@ function WeatherDetailPage() {
         );
         const [locationData] = await geocodingResponse.json();
 
-        // chiamata api principale
-        const weatherResponse = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${locationData.lat}&lon=${locationData.lon}&appid=${apiKey}`
-        );
-        const weatherData = await weatherResponse.json();
+        // controllo x geocoding
+        if (locationData) {
+          // chiamata api principale
+          const weatherResponse = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${locationData.lat}&lon=${locationData.lon}&appid=${apiKey}`
+          );
+          const weatherData = await weatherResponse.json();
+          setWeatherData(weatherData);
 
-        setWeatherData(weatherData);
-        setLoading(false);
+          // chiamata api previsioni
+          const forecastResponse = await fetch(
+            `https://api.openweathermap.org/data/2.5/forecast?lat=${locationData.lat}&lon=${locationData.lon}&appid=${apiKey}`
+          );
+          const forecastData = await forecastResponse.json();
+          setForecastData(forecastData);
+        }
       } catch (err) {
         setError(err.message);
-        setLoading(false);
       }
+      setLoading(false);
     };
 
     fetchWeatherData();
@@ -50,9 +88,23 @@ function WeatherDetailPage() {
           <p>T now: {kelvinToCelsius(weatherData.main.temp)}°C</p>
           <p>T min: {kelvinToCelsius(weatherData.main.temp_min)}°C</p>
           <p>T max: {kelvinToCelsius(weatherData.main.temp_max)}°C</p>
+          <p>hum: {weatherData.main.humidity}%</p>
           <p>w: {weatherData.weather[0].description}</p>
+          <p>Sunrise: {timeToLocaleTime(weatherData.sys.sunrise)}</p>
+          <p>Sunset: {timeToLocaleTime(weatherData.sys.sunset)}</p>
         </div>
       )}
+      <h2>Previsioni dei prossimi 3 giorni</h2>
+      {forecastData &&
+        getDailyForecasts(forecastData).map((forecast, index) => (
+          <div key={index}>
+            <p>Giorno: {new Date(forecast.dt * 1000).toLocaleDateString()}</p>
+            <p>Temperatura: {kelvinToCelsius(forecast.main.temp)}°C</p>
+            <p>Temperatura Minima: {kelvinToCelsius(forecast.main.temp_min)}°C</p>
+            <p>Temperatura Massima: {kelvinToCelsius(forecast.main.temp_max)}°C</p>
+            <p>Condizioni: {forecast.weather[0].description}</p>
+          </div>
+        ))}
     </div>
   );
 }
